6925963fe33951e41349387583007c6fe790698c
arzedlab <arzedlab@gmail.com>
2021-08-25 23:33:51 +0500

    app.py updated

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ async def read_item(id):
         return {"error": "ID not found"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
     
